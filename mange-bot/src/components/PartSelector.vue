2f37f9619d342e4986c3df4ee60def4ce51a1db1
RaphaelRSantos02 <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-10-17 21:48:30 -0300

    Update PartSelector.vue

--- mange-bot/src/components/PartSelector.vue
+++ mange-bot/src/components/PartSelector.vue
@@ -50,7 +50,7 @@ const changePart = (isNext: boolean)=>{
 </script>
 <template>
     <div :class="`part ${props.position}`" v-if="props.parts[selectedIndex]">          
-        <img :src="BASE_URL + props.parts[selectedIndex]?.src" alt="">
+        <img class="part-images" :image-id="props.parts[selectedIndex].id" :src="BASE_URL + props.parts[selectedIndex]?.src" alt="">
         <button @click="changePart(false)" class="prev-selector"></button>
         <button @click="changePart(true)" class="next-selector"></button>
     </div>
